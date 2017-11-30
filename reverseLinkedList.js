773c54d81c68e5f028f4e8c28d0e409136c2e007
Yilin Xu <yilin.xu@mastercard.com>
2017-11-30 10:38:08 -0500

    add rvs linked list notes

--- reverseLinkedList.js
+++ reverseLinkedList.js
@@ -3,16 +3,22 @@ function Node(val) {
   this.next = null;
 }
 
+// each node in linked list holds a value and a next reference to the next node of the linked List, or null
 
 var a = new Node('a');
 var b = a.next = new Node('b');
 var c = b.next = new Node('c');
 var d = c.next = new Node('d');
 
+// a---b---c---d---null
 
 
-//reverse Linked List in Place
+// Function for reversing a linked List so result is
+// 
+// d---c---b---a---null
 
+// Solution: keep 3 pointers and iterate through list, in each iteration
+// change current nodes next to prev, 
 function reverseLL(rootNode) {
   var prevNode = null;
   var currNode = rootNode;
@@ -28,3 +34,57 @@ function reverseLL(rootNode) {
 }
 
 console.log(reverseLL(a));
+
+// First iteration
+// nextNode = currNode.next, assign nextNode pointer to B
+
+// P      C   N
+// null   a -> b -> c -> d -> null
+
+// currNode.next = prevNode
+
+//  C      P      N
+//  a -> null     b -> c -> d -> null
+
+//
+// prevNode = currNode, now need to make A the "new" previousNode
+
+//  C
+//  P             N
+//  a -> null     b -> c -> d -> null
+
+// currNode = nextNode, and now currNode needs to refer to the "old" nextNode, B
+
+//                C
+//  P             N
+//  a -> null     b -> c -> d -> null
+
+// and the while loop iterates again until the end of the list.
+// 2nd iteration
+// 
+// nextNode = currNode.next
+//                C
+//  P                  N
+//  a -> null     b -> c -> d -> null
+
+// currNode.next = prevNode;
+//  C    P            N
+//  b -> a -> null    c -> d -> null
+
+
+// prevNode = currNode, now need to make B the "new" previousNode
+
+//  C
+//  P                 N
+//  b -> a -> null    c -> d -> null
+
+// currNode = nextNode, and now currNode needs to refer to the "old" nextNode, C
+
+//                    C
+//  P                 N
+//  b -> a -> null    c -> d -> null
+
+// go into loop again, eventually prev Node will be pointing to D (the new head) after while loop ends
+// and we return that
+// d --> c --> b-->a --Null
+
